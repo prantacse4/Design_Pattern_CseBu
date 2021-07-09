0b7da8bc3b0c8a34c4eacbf22d190ecb64a05cd7
prantacse4 <pranta.cse4.bu@gmail.com>
2021-07-09 16:04:15 +0600

    MVC

---
+++ src/MVC_01/MVC_PatternDemo.java
@@ -0,0 +1,38 @@
+package MVC_01;
+
+public class MVC_PatternDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		StudentModel model = CreateStudentDatase();
+		StudentView view = new StudentView();
+		StudentController controller = new StudentController(model, view);
+		
+		System.out.println("Main from Database");
+		controller.ViewFunction();
+		
+		System.out.println("\n Updating Name...");
+		controller.setStuName("My Full Name");
+		controller.ViewFunction();
+		
+		System.out.println("\n Updating Roll...");
+		controller.setStuName("My Class Roll");
+		controller.ViewFunction();
+
+		
+		System.out.println("\n Updating Dept...");
+		controller.setStuName("Computer Science and Engineering");
+		controller.ViewFunction();
+
+
+	}
+	public static StudentModel CreateStudentDatase() {
+		StudentModel student = new StudentModel();
+		student.setName("Name");
+		student.setRoll("MyRoll");
+		student.setDept("CSE");
+		return student;
+	}
+
+}
---
+++ src/MVC_01/StudentController.java
@@ -0,0 +1,40 @@
+package MVC_01;
+
+public class StudentController {
+	private StudentModel model;
+	private StudentView view;
+	public StudentController(StudentModel model, StudentView view) {
+		super();
+		this.model = model;
+		this.view = view;
+	}
+	
+	public void setStuName(String name) {
+		model.setName(name);
+	}
+	public void setStuRoll(String roll) {
+		model.setRoll(roll);
+	}
+	public void setStuDept(String dept) {
+		model.setDept(dept);;
+	}
+	
+	public String getStuName() {
+		return model.getName();
+	}
+	
+	public String getStuRoll() {
+		return model.getRoll();
+	}
+	
+	public String getStuDept() {
+		return model.getDept();
+	}
+	
+	public void ViewFunction() {
+		view.viewStudent(model.getName(), model.getRoll(), model.getDept());
+	}
+	
+	
+
+}
