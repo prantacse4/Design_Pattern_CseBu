fff46d6b3cc374b45803abf3af9f08432715fd2f
prantacse4 <pranta.cse4.bu@gmail.com>
2021-07-12 18:13:24 +0600

    Update

---
+++ src/Composite_Design_Pattern_02/MediaPlayer.java
@@ -0,0 +1,6 @@
+package Composite_Design_Pattern_02;
+
+public interface MediaPlayer {
+	void decoderDetails();
+
+}
---
+++ src/Composite_Design_Pattern_02/Media_Composite.java
@@ -0,0 +1,5 @@
+package Composite_Design_Pattern_02;
+
+public class Media_Composite {
+
+}
